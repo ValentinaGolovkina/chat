8de35d704b7c855240e2ee6be53d1e6a99ac84f8
ValentinaGolovkina <gvalya.25@gmail.com>
2021-02-26 16:16:53 +0300

    Created main window with field for message

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+
+    @FXML
+    public TextArea messagesTA;
+    @FXML
+    public TextField messageTF;
+
+    public void onClickBthSend(ActionEvent actionEvent) {
+        sendMessage();
+    }
+
+    public void onPressedEnter(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            sendMessage();
+        }
+    }
+
+    private void sendMessage(){
+        String message = messageTF.getText();
+        if(!message.equals("")){
+            messagesTA.appendText(messageTF.getText() +"\n");
+            messageTF.clear();
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello world");
+        primaryStage.setTitle("Online chat");
         primaryStage.setScene(new Scene(root, 700, 800));
         primaryStage.show();
     }
